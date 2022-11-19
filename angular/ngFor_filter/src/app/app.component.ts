72b7120a6d2eea14ccfcc62b6cc83f20eaae5f5a
Rathek <aempinto02@gmail.com>
2022-11-19 11:12:16 -0300

    Adição de pesquisa e autocompletar

--- angular/ngFor_filter/src/app/app.component.ts
+++ angular/ngFor_filter/src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface Vault {
+  vault: string;
+  transactions: Transaction[];
+}
+
+export interface Transaction {
+  balance: number;
+  asset: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +19,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngFor_filter';
+
+  vaults: Vault[] = [
+    {
+      vault: 'a',
+      transactions: [
+        {
+          balance: 1,
+          asset: 'BTC'
+        },
+        {
+          balance: 2,
+          asset: 'BTC'
+        },
+        {
+          balance: 3,
+          asset: 'ETH'
+        },
+      ],
+    },
+    {
+      vault: 'b',
+      transactions: [
+        {
+          balance: 1,
+          asset: 'BTC'
+        },
+        {
+          balance: 2,
+          asset: 'BTC'
+        },
+        {
+          balance: 3,
+          asset: 'ETH'
+        },
+      ],
+    },
+    {
+      vault: 'c',
+      transactions: [
+        {
+          balance: 1,
+          asset: 'BTC'
+        },
+        {
+          balance: 2,
+          asset: 'BTC'
+        },
+      ]
+    },
+  ]
+
+  displayedColumns: string[] = ['vault', 'balance', 'status'];
+  dataSource = new MatTableDataSource(this.vaults);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  searchText = '';
+  characters = ['a','b','c']
 
   status = ['em processamento', 'cancelado', 'concluído']
 
--- angular/ngFor_filter/src/app/app.module.ts
+++ angular/ngFor_filter/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FilterPipe } from './utils/filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
